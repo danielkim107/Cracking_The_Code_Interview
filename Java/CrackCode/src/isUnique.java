d72ede56a4d4e085d67a6e847213a65da0be18a3
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2018-06-18 13:45:21 +0900

    Java Time

---
+++ Java/CrackCode/src/isUnique.java
@@ -0,0 +1,43 @@
+import java.util.Hashtable;
+
+/**
+ * <h1> Is Unique </h1>
+ * Interview Question 1.1
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ * 
+ * @author danielkim107
+ */
+
+public class isUnique {
+	
+	/**
+	 * Tests whether the current String input contains all unique characters.
+	 * @param str
+	 * @return boolean of whether str has all unique characters
+	 */
+	public static boolean isUniqueHash(String str) {
+		Hashtable<Character, Integer> ht = new Hashtable<>();
+		boolean answer = false;
+		
+		for (int i = 0; i < str.length(); i++) {
+			char curr = str.charAt(i);
+			if (ht.containsKey(curr)) {
+				answer = true;
+			} else {
+				ht.put(curr, 1);
+			}
+		}
+		
+		return answer;
+		
+	}
+	
+	public static void main(String str[]) {
+		String test = "ababa";
+		boolean ans = isUniqueHash(test);
+		
+		System.out.println(ans);;
+	}
+
+}
